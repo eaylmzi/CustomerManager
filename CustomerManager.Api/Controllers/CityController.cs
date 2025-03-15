07158d2dbdeb2c10d58eaf6403c42c299023b521
eaylmzi <emreyilmaz0999@hotmail.com>
2025-03-16 00:50:09 +0300

    Configure project references and add api layer

---
+++ CustomerManager.Api/Controllers/CityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerManager.Api.Controllers
+{
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CustomerManager.Api/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerManager.Api.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
